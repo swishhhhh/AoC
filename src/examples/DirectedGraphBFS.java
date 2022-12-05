88bb221c06e0452a2c7f514005a7d9139146f5f8
zsalzer <zsalzer@yahoo.com>
2022-12-04 23:57:39 -0500

    examples: directed graph with bfs and dfs

--- src/examples/DirectedGraphBFS.java
+++ src/examples/DirectedGraphBFS.java
@@ -16,7 +16,7 @@ class DirectedGraphBFS
     DirectedGraphBFS(int v)
     {
         V = v;
-        adj = new LinkedList<>[v];
+        adj = new LinkedList[v];
         for (int i=0; i<v; ++i)
             adj[i] = new LinkedList<>();
     }
