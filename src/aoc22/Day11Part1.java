8477932c0819ebb63d0d864040b0399736cadf5d
zsalzer <zsalzer@yahoo.com>
2022-12-11 04:39:21 -0500

    AoC 2022 Day11 - working (finally!!)

--- src/aoc22/Day11Part1.java
+++ src/aoc22/Day11Part1.java
@@ -3,21 +3,21 @@ package aoc22;
 import utils.Helper;
 import utils.ResourceLoader;
 
-import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Queue;
 import java.util.concurrent.LinkedBlockingQueue;
 
-public class Day11Part2 {
+public class Day11Part1 {
 	static class Monkey {
 		public Monkey(String name) {
 			this.name = name;
 		}
 
 		public String name;
-		public Queue<Long> items = new LinkedBlockingQueue<>();
-		public long inspectedCtr = 0;
+		public Queue<Integer> items = new LinkedBlockingQueue<>();
+//		public int level;
+		public int inspectedCtr = 0;
 		public String operator1;
 		public String operator2;
 		public String operand;
@@ -30,6 +30,7 @@ public class Day11Part2 {
 			return "Monkey{" +
 					"name='" + name + '\'' +
 					", items=" + items +
+//					", level=" + level +
 					", inspectedCtr=" + inspectedCtr +
 					", operator1='" + operator1 + '\'' +
 					", operator2='" + operator2 + '\'' +
@@ -47,7 +48,6 @@ public class Day11Part2 {
 	public static void main(String[] args) throws Exception {
 		List<String> lines = ResourceLoader.readStrings("aoc22/Day11_input.txt");
 		Monkey m = null;
-		long primeReducer = 1;
 
 		for (String line: lines) {
 			//parse input
@@ -57,9 +57,7 @@ public class Day11Part2 {
 				monkeys.add(m);
 			} else if (line.startsWith("Starting items:")) {
 				List<Integer> nums = Helper.extractIntsFromText(line);
-				for (Integer n: nums) {
-					m.items.add((long)n);
-				}
+				m.items.addAll(nums);
 			} else if (line.trim().startsWith("Operation:")) {
 				String[] ary = line.split(" ");
 				m.operator1 = ary[ary.length - 3];
@@ -67,7 +65,6 @@ public class Day11Part2 {
 				m.operator2 = ary[ary.length - 1];
 			} else if (line.startsWith("Test:")) {
 				m.divisibleNum = Helper.extractIntsFromText(line).get(0);
-				primeReducer*= m.divisibleNum;
 			} else if (line.startsWith("If true:")) {
 				m.throwToMonkeyNumIfTrue = Helper.extractIntsFromText(line).get(0);
 			} else if (line.startsWith("If false:")) {
@@ -76,31 +73,30 @@ public class Day11Part2 {
 		}
 
 		//rounds
-		for (int round = 1; round <= 10_000; round++) {
+		for (int round = 1; round <= 20; round++) {
 			for (Monkey monkey: monkeys) {
 				int size = monkey.items.size();
 				for (int i = 0; i < size; i++) {
 					monkey.inspectedCtr++;
-					Long itemLevel = monkey.items.poll();
-					long op1 = Helper.isNumeric(monkey.operator1) ? Long.parseLong(monkey.operator1) : itemLevel;
-					long op2 = Helper.isNumeric(monkey.operator2) ? Long.parseLong(monkey.operator2) : itemLevel;
-					long newLevel = monkey.operand.equals("+") ? op1 + op2 : op1 * op2;
+					int itemLevel = monkey.items.poll();
+					int op1 = Helper.isNumeric(monkey.operator1) ? Integer.parseInt(monkey.operator1) : itemLevel;
+					int op2 = Helper.isNumeric(monkey.operator2) ? Integer.parseInt(monkey.operator2) : itemLevel;
+					int newLevel = monkey.operand.equals("+") ? op1 + op2 : op1 * op2;
+					newLevel = newLevel / 3;
 
-					newLevel = newLevel % primeReducer;
-					long remainder = newLevel % monkey.divisibleNum;
-					int throwToMonkeyNum = remainder == 0L ? monkey.throwToMonkeyNumIfTrue : monkey.throwToMonkeyNumIfFalse;
+					int throwToMonkeyNum =
+							newLevel % monkey.divisibleNum == 0 ? monkey.throwToMonkeyNumIfTrue : monkey.throwToMonkeyNumIfFalse ;
 
 					Monkey throwToMonkey = monkeys.get(throwToMonkeyNum);
 					throwToMonkey.items.add(newLevel);
 				}
-
 			}
 		}
 
 		monkeys.forEach(System.out::println);
 
 		//figure out 2 highest monkey inspectors
-		long highest = 0L, secondHighest = 0L;
+		int highest = 0, secondHighest = 0;
 		for (Monkey monkey: monkeys) {
 			if (monkey.inspectedCtr > highest) {
 				secondHighest = highest;
@@ -110,6 +106,6 @@ public class Day11Part2 {
 			}
 		}
 
-		System.out.printf("Total = %s%n", BigInteger.valueOf(highest).multiply(BigInteger.valueOf(secondHighest)));
+		System.out.printf("Total = %s%n", highest * secondHighest);
 	}
 }
