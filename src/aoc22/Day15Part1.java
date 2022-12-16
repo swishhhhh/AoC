e5c76e0eb862b15adf83e79daee8085a8c12eebc
zsalzer <zsalzer@yahoo.com>
2022-12-16 02:23:00 -0500

    AoC 2022 Day 15 - optimized/upgraded part1 to use same diamond algo developed for part2.

--- src/aoc22/Day15Part1.java
+++ src/aoc22/Day15Part1.java
@@ -1,6 +1,7 @@
 package aoc22;
 
 import aoc22.datastructs.Coordinates;
+import aoc22.datastructs.Diamond;
 import utils.Helper;
 import utils.ResourceLoader;
 
@@ -11,6 +12,8 @@ import java.util.Set;
 public class Day15Part1 {
 	static Set<Coordinates> selectedCoords = new LinkedHashSet<>();
 	static Set<Coordinates> beacons = new LinkedHashSet<>();
+	static Set<Diamond> diamonds = new LinkedHashSet<>();
+
 	static int targetRow = 2000000;
 
 	public static void main(String[] args) throws Exception {
@@ -19,6 +22,7 @@ public class Day15Part1 {
 		int ctr = 0;
 
 		int sensorX, sensorY, beaconX, beaconY;
+		int minX = Integer.MAX_VALUE, maxX = Integer.MIN_VALUE;
 		for (String line: lines) {
 			ctr++;
 			List<Integer> ints = Helper.extractIntsFromText(line, true);
@@ -29,25 +33,23 @@ public class Day15Part1 {
 			Coordinates beacon = new Coordinates(beaconX, beaconY);
 			beacons.add(beacon);
 			int distance = Math.abs(sensorX - beaconX) + Math.abs(sensorY - beaconY);
-			int verticalDistanceToTargetRow = Math.abs(targetRow - sensorY);
-			int adjDistance = distance - verticalDistanceToTargetRow + 1; //only count between rows
+			Diamond diamond = new Diamond(sensorX, sensorY, distance + 1);
+			diamonds.add(diamond);
+			minX = Math.min(minX, sensorX - distance);
+			maxX = Math.max(maxX, sensorX + distance);
 
-			System.out.printf("line %s: %s, distance: %s, adjusted distance: %s%n", ctr, line, distance, adjDistance);
+			System.out.printf("line %s: %s, distance: %s%n", ctr, line, distance);
+		}
 
-			for (int i = 0; i < adjDistance; i++) {
-				Coordinates coord = new Coordinates(sensorX - i, targetRow); //left coord (or center when i == 0)
-				if (!coord.equals(beacon)) {
-					selectedCoords.add(coord);
-				}
-
-				if (i > 0) {
-					coord = new Coordinates(sensorX + i, targetRow); //right coord
-					if (!coord.equals(beacon)) {
-						selectedCoords.add(coord);
-					}
+		for (int i = minX; i <= maxX; i++) {
+			for (Diamond d: diamonds) {
+				if (d.containsPoint(i, targetRow)) {
+					selectedCoords.add(new Coordinates(i, targetRow));
 				}
 			}
 		}
+
+		selectedCoords.removeAll(beacons);
 
 		System.out.printf("Selected Coordinates = %s%n", selectedCoords.size());
 	}
