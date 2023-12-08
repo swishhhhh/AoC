5f6ef64e504d3dae7fd2cffbdb465a1ed114314d
zsalzer <zsalzer@yahoo.com>
2023-12-08 01:42:46 -0500

    Aoc 2023 - Day 8

---
+++ src/aoc23/Day8Part1.java
@@ -0,0 +1,56 @@
+package aoc23;
+
+import utils.ResourceLoader;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * <a href="https://adventofcode.com/2023/day/8">Advent of Code 2023 Day 8</a>
+ */
+public class Day8Part1 {
+
+	public static void main(String[] args) throws Exception {
+		List<String> lines = ResourceLoader.readStrings("aoc23/Day8_input.txt");
+
+		char[] leftRightAry = lines.get(0).toCharArray();
+		Map<String, String[]> nodes = new HashMap<>();
+
+		for (int i = 2; i < lines.size(); i++) {
+			String line = lines.get(i);
+			String key = line.split("=")[0].trim();
+			String values = line.split("=")[1].trim();
+			values = values.replace("(", "").replace(")", "");
+			String[] valsAry = values.split(",");
+			valsAry[1] = valsAry[1].trim();
+			nodes.put(key, valsAry);
+		}
+
+		int lrPtr = 0;
+		String nodePtr = "AAA";
+		long steps = 0;
+		while (true) {
+			if (nodePtr.equals("ZZZ")) {
+				break;
+			}
+
+			String[] values = nodes.get(nodePtr);
+			if (leftRightAry[lrPtr] == 'L') {
+				nodePtr = values[0];
+			} else {
+				nodePtr = values[1];
+			}
+
+			steps++;
+			lrPtr++;
+			if (lrPtr >= leftRightAry.length) {
+				lrPtr = 0;
+			}
+		}
+
+
+		System.out.printf("Steps = %s%n", steps);
+	}
+
+}
---
+++ src/aoc23/Day8Part2.java
@@ -0,0 +1,96 @@
+package aoc23;
+
+import utils.Helper;
+import utils.ResourceLoader;
+
+import java.util.*;
+
+/**
+ * <a href="https://adventofcode.com/2023/day/8">Advent of Code 2023 Day 8</a>
+ */
+public class Day8Part2 {
+
+	public static void main(String[] args) throws Exception {
+		List<String> lines = ResourceLoader.readStrings("aoc23/Day8_input.txt");
+
+		char[] leftRightAry = lines.get(0).toCharArray();
+		Map<String, String[]> nodes = new HashMap<>();
+
+		for (int i = 2; i < lines.size(); i++) {
+			String line = lines.get(i);
+			String key = line.split("=")[0].trim();
+			String values = line.split("=")[1].trim();
+			values = values.replace("(", "").replace(")", "");
+			String[] valsAry = values.split(",");
+			valsAry[1] = valsAry[1].trim();
+			nodes.put(key, valsAry);
+		}
+
+		//find all start and end nodes
+		Set<String> startNodes = new HashSet<>();
+		for (String node : nodes.keySet()) {
+			if (node.endsWith("A")) {
+				startNodes.add(node);
+			}
+		}
+
+		long steps = executeCycles(leftRightAry, nodes, startNodes);
+		System.out.printf("Steps = %s%n", steps);
+	}
+
+	private static long executeCycles(char[] leftRightAry, Map<String, String[]> nodes, Set<String> startNodes) {
+		int lrPtr = 0;
+		String[] nodePtrs = startNodes.toArray(new String[]{});
+
+		//keep track of cycle lengths for each path
+		long[] cycleLengths = new long[startNodes.size()];
+		long cyclesFoundCtr = 0;
+
+		long steps = 0;
+
+		while (true) {
+			if (allPtrsAtEnd(nodePtrs)) {
+				break;
+			}
+
+			for (int i = 0; i < nodePtrs.length; i++) {
+				String nodePtr = nodePtrs[i];
+
+				if (nodePtr.endsWith("Z") && cycleLengths[i] == 0) {
+					cycleLengths[i] = steps;
+					cyclesFoundCtr++;
+				}
+
+				//if cycle length found (found node ending with "Z") for every pointer/path, then short-circuit by
+				// getting lowest common multiplier of all cycle lengths which will == the total number of steps needed
+				if (cyclesFoundCtr == startNodes.size()) {
+					return Helper.lowestCommonMultiplier(cycleLengths);
+				}
+
+				String[] values = nodes.get(nodePtr);
+				if (leftRightAry[lrPtr] == 'L') {
+					nodePtrs[i] = values[0];
+				} else {
+					nodePtrs[i] = values[1];
+				}
+			}
+
+			steps++;
+			lrPtr++;
+			if (lrPtr >= leftRightAry.length) {
+				lrPtr = 0;
+			}
+		}
+
+		return steps;
+	}
+
+	private static boolean allPtrsAtEnd(String[] nodePtrs) {
+		for (String node : nodePtrs) {
+			if (!node.endsWith("Z")) {
+				return false;
+			}
+		}
+		return true;
+	}
+}
