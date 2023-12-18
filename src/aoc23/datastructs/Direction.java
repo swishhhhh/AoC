ddc77004aa093dfcd852d6ccdc9164e4a6523c7b
zsalzer <zsalzer@yahoo.com>
2023-12-17 19:40:34 -0500

    added isReverseOf method

--- src/aoc23/datastructs/Direction.java
+++ src/aoc23/datastructs/Direction.java
@@ -14,6 +14,21 @@ public enum Direction {
         this.symbol = symbol;
     }
 
+    public boolean isReverseOf(Direction otherDirection) {
+        boolean answer = false;
+        switch (otherDirection) {
+            case NORTH -> answer = this.equals(SOUTH);
+            case SOUTH -> answer = this.equals(NORTH);
+            case EAST -> answer = this.equals(WEST);
+            case WEST -> answer = this.equals(EAST);
+            case NE -> answer = this.equals(SW);
+            case SW -> answer = this.equals(NE);
+            case NW -> answer = this.equals(SE);
+            case SE -> answer = this.equals(NW);
+        }
+        return answer;
+    }
+
     @Override
     public String toString() {
         return "Direction{" +
