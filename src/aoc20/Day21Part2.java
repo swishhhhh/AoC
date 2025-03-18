15dd6eb0b71d72c327ea424b6cd721395779f64a
zsalzer <zsalzer@yahoo.com>
2025-03-18 16:23:30 -0400

    Aoc 2020 - Day 21

---
+++ src/aoc20/Day21Part2.java
@@ -0,0 +1,101 @@
+package aoc20;
+
+import org.apache.commons.math3.util.Pair;
+import utils.ResourceLoader;
+
+import java.util.*;
+
+/**
+ * <a href="https://adventofcode.com/2020/day/21">Advent of Code 2020 Day 21</a>
+ */
+public class Day21Part2 {
+    public static void main(String... args) throws Exception {
+        List<String> lines = ResourceLoader.readStrings("aoc20/Day21_input.txt");
+
+        String answer = new Day21Part2().execute(lines);
+        System.out.printf("Answer = %s%n", answer);
+
+        String expected = "lkv,lfcppl,jhsrjlj,jrhvk,zkls,qjltjd,xslr,rfpbpn";
+        if (!answer.equals(expected)) {
+            throw new RuntimeException(String.format("Answer %s doesn't match expected %s", answer, expected));
+        }
+    }
+
+    private String execute(List<String> lines) {
+        List<Pair<List<String>, List<String>>> input = parseInput(lines);
+
+        Map<String, String> ingredientToAllergenMap = new HashMap<>();
+        Map<String, Set<String>> allergenToPossibleIngredientsMap = new HashMap<>();
+
+        for (Pair<List<String>, List<String>> pair : input) {
+            Set<String> ingredients = new HashSet<>(pair.getFirst());
+            for (String allergen : pair.getSecond()) {
+                evaluateAllergenInput(allergen, ingredients, ingredientToAllergenMap, allergenToPossibleIngredientsMap);
+            }
+        }
+
+        return selectAndSortBadIngredients(ingredientToAllergenMap);
+    }
+
+    private List<Pair<List<String>, List<String>>> parseInput(List<String> lines) {
+        return lines.stream()
+                .map(line -> {
+                    String[] parts = line.split(" \\(contains ");
+                    String[] ingredients = parts[0].split(" ");
+                    String[] allergens = parts[1].replace(")", "").split(", ");
+                    return new Pair<>(List.of(ingredients), List.of(allergens));
+                })
+                .toList();
+    }
+
+    private void evaluateAllergenInput(String allergen, Set<String> possibleIngredients,
+                                       Map<String, String> ingredientToAllergenMap,
+                                       Map<String, Set<String>> allergenToPossibleIngredientsMap) {
+        //remove ingredients already matched to allergens (are keys in ingredientToAllergenMap)
+        possibleIngredients.removeAll(ingredientToAllergenMap.keySet());
+
+        //if allergen already seen, retain only a cross-section of its initial possible-ingredients and the new possible-ingredients
+        if (allergenToPossibleIngredientsMap.containsKey(allergen)) {
+            allergenToPossibleIngredientsMap.get(allergen).retainAll(possibleIngredients);
+        } else { //first time allergen is seen
+            allergenToPossibleIngredientsMap.put(allergen,  new HashSet<>(possibleIngredients));
+        }
+
+        while (true) {
+            /*
+              for each allergen -> list-of-possible-ingredients
+                 if list-of-possible-ingredients has only 1 ingredient
+                     add to ingredientToAllergenMap
+                     remove ingredient from all other lists-of-possible-ingredients (values in allergenToPossibleIngredientsMap)
+                     singleFound = true
+            */
+            String singleIngredient = allergenToPossibleIngredientsMap.entrySet().stream()
+                    .filter(entry -> entry.getValue().size() == 1)
+                    .findFirst()
+                    .map(entry -> {
+                        String foundIngredient = entry.getValue().iterator().next();
+                        ingredientToAllergenMap.put(foundIngredient, entry.getKey());
+                        return foundIngredient;
+                    })
+                    .orElse(null);
+
+            if (singleIngredient == null) {
+                break;
+            }
+
+            // Remove the found ingredient from all possible ingredients sets
+            allergenToPossibleIngredientsMap.values()
+                    .forEach(set -> set.remove(singleIngredient));
+        }
+    }
+
+    private String selectAndSortBadIngredients(Map<String, String> ingredientToAllergenMap) {
+        //return a comma-separated list of ingredients sorted by their allergen
+        Map<String, String> allergenToIngredientMap = new TreeMap<>(); //sorted map
+        for (Map.Entry<String, String> entry : ingredientToAllergenMap.entrySet()) {
+            allergenToIngredientMap.put(entry.getValue(), entry.getKey());
+        }
+
+        return String.join(",", allergenToIngredientMap.values());
+    }
+}
